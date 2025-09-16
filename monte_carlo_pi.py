719f9eb38dfd025f514984eec8e9f30a19780e0a
Brian <55033153+subtotechnoblade@users.noreply.github.com>
2025-09-15 23:48:32 -0400

    Update monte_carlo_pi.py
    renamed vars

--- monte_carlo_pi.py
+++ monte_carlo_pi.py
@@ -2,14 +2,14 @@ import numba as nb
 import numpy as np
 import multiprocessing as mp
 @nb.njit(nogil=True, cache=True, parallel=True)
-def mcts_pi(visits):
-    wins = 0
-    for _ in nb.prange(visits):
+def mcts_pi(iteration_limit):
+    hits = 0
+    for _ in nb.prange(iteration_limit):
         x = np.random.random()
         y = np.random.random()
         if x**2 + y**2 <= 1:
-            wins += 1
-    return 4.0 * wins / visits
+            hits += 1
+    return 4.0 * hits / iteration_limit
 
 if __name__ == "__main__":
     # print(mcts_pi.parallel_diagnostics(level=4))
